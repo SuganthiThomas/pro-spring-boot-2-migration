05131dab290551bcd8ed2ec4284f3a026d356030
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2025-09-06 00:41:53 -0600

    Add migrated todo-redis app
    todo-redis app works succesfully with a local server - a Docker instance

---
+++ ch05/todo-redis/src/main/java/com/apress/todo/domain/ToDo.java
@@ -0,0 +1,41 @@
+package com.apress.todo.domain;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@RedisHash
+public class ToDo {
+    @NotNull
+    @Id
+    private String id;
+    @NotNull
+    @NotBlank
+    private String description;
+    private LocalDateTime created;
+    private LocalDateTime modified;
+    private boolean completed;
+    
+    
+    public ToDo() {
+        LocalDateTime date = LocalDateTime.now();
+        this.id = UUID.randomUUID().toString();
+        this.created = date;
+        this.modified = date;
+    }
+    
+    public ToDo(String description) {
+        this();
+        this.description = description;
+    }
+}
+
