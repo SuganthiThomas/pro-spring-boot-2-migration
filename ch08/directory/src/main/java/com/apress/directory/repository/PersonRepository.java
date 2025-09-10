012d77d96f96b572f0318925647c6cd9fb6f2ce7
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2025-09-10 14:19:15 -0600

    Remove old files from Chapter 8

--- ch08/directory/src/main/java/com/apress/directory/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apress.directory.repository;
-
-import com.apress.directory.domain.Person;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface PersonRepository extends CrudRepository<Person,String> {
-    public Person findByEmailIgnoreCase(@Param("email") String email);
-}
