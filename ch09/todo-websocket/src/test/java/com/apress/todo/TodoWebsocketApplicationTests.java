30cae20924dc4ceef62341a4f0f101599accd298
Gerardo Pérez Pérez <jerryperezxbox@gmail.com>
2025-09-15 19:33:07 -0600

    Adds todo-websocket app

---
+++ ch09/todo-websocket/src/test/java/com/apress/todo/TodoWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoWebsocketApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
